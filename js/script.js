92a888e23701c49c6a1db0e2de124048525280f6
simeon <simeonpitts@gmail.com>
2023-07-19 15:52:42 -0700

    i have added the function for the totalcost and the array for topings

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-function pizza(size, price, topping,) {
+function Pizza(size, topping,) {
     this.size = size;
-    this.price = price;
     this.topping = topping;
 }
-let newpizza = new pizza("med", 5.99, "pepperoni");
+
+this.topping.array.forEach(element => {
+    switch (element){
+        case ("mushrooms"):
+        case ("pinapple"):
+        case ("pepperoni"):
+            total += .50
+    } 
+});
+
+Pizza.prototype.totalcost = function() {
+    let total = 0.00;
+    if (this.size === "small"){
+        return $2 
+    } else if (this.size === "medium"){
+        return $3
+    } else (this.size === "large");{
+        return $4
+    }
+}
+
+function getInputValue(inputArray){
+    const innerTextArray = [];
+    inputArray.forEach(function(element){
+      const elementString = element.value;
+      innerTextArray.push(elementString);
+    });
+    return innerTextArray;
+  }
