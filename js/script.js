b3abc49e8728a84f1f65da6f66260d8c8ccdea53
simeon <simeonpitts@gmail.com>
2023-07-28 21:12:18 -0700

    I have changed the js to a class type instead of a function

--- js/script.js
+++ js/script.js
@@ -1,51 +1,54 @@
 
 // bussiness logic //
 class Pizza {
-    cunstructor(size, toping){
+    constructor(size, toping){
         this.size= size;
         this.topping= topping;
+        this.price=0;
     }
+    addSize() {
+        if (this.size == "small"){
+            this.price += 6.99;
+        } else if (this.size == "medium"){
+            this.price+=8.49;
+        } else if (this.size == "large"){
+            this.price+=12.75;
+        } else {
+            console.error("this is not an option");
+        }
+    }
+    addToppings () {
+        if (this.topping == "pepperoni"){
+            this.price +=2.99 ;
+        } else if (this.topping == "pinapple"){
+            this.price += 2.99;
+        } else if (this.topping == "mushroom"){
+            this.price += 2.49;
+        } else {
+            return false;
+        }
+    }
+    
 }
 
-const pizza1 = new Pizza("");
+const myPizza = new Pizza('medium', ['cheese', 'pepperoni']);
+myPizza.addSize();
 
-pizza1.topping.forEach(function(element) {
-    switch (element){
-        case ("mushrooms"):
-        case ("pinapple"):
-        case ("pepperoni"):
-            total += .50
-    } 
-});
+console.log(myPizza.price)
 
-let mainCost = 0;
 
-switch (pizza1.size){
-    case "small":
-        mainCost = 6.99;
-        break;
-    case "medium":
-        mainCost = 8.49;
-        break;
-    case "large":
-        mainCost = 10.29;
-        break;d
-}
+
 
 // UI logic //
 
-window.addEventListener("load", function(){
-    this.document.querySelector("from#order-list").addEventListener("submit")
-});
+// window.addEventListener("load", function(){
+//     this.document.querySelector("from#order-list").addEventListener("submit")
+// });
 
-function OnSubmit(event){
-    event.preventDefault();
-    const orderListing = document.getElementById("OrderList");
-    orderListing.innerHTML="";
-    const Topping = document.querySelector("input[name='toping']:checked").value;
-    const Size = document.querySelector("input[name='size']:checked").value;
+// function OnSubmit(event){
+//     event.preventDefault();
+//     const orderListing = document.getElementById("OrderList");
+//     orderListing.innerHTML="";
+//     const Topping = document.querySelector("input[name='toping']:checked").value;
+//     const Size = document.querySelector("input[name='size']:checked").value;
     
-
-
-    
-}
