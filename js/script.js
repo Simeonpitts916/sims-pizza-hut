44934fb8de6bb1986db5c616a8be93a97e562230
simeon <simeonpitts@gmail.com>
2023-07-28 18:42:45 -0700

    have added the window and the on submit to the script file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+
+// bussiness logic //
 function Pizza(size, topping,) {
     this.size = size;
     this.topping = topping;
 }
 
-this.topping.array.forEach(element => {
+this.topping.forEach(function(element) {
     switch (element){
         case ("mushrooms"):
         case ("pinapple"):
@@ -23,11 +25,18 @@ Pizza.prototype.totalcost = function() {
     }
 }
 
-function getInputValue(inputArray){
-    const innerTextArray = [];
-    inputArray.forEach(function(element){
-      const elementString = element.value;
-      innerTextArray.push(elementString);
-    });
-    return innerTextArray;
-  }
+// UI logic //
+
+window.addEventListener("load", function(){
+    this.document.querySelector("from#order-list").addEventListener("submit")
+});
+
+function OnSubmit(event){
+    event.preventDefault();
+    const orderListing = document.getElementById("OrderList");
+    orderListing.innerHTML="";
+    const topping = document.querySelector("input[name='toping']:checked").value;
+    
+
+    
+}
